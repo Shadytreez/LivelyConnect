f739c904a63f106072f77b658413e07863ee274f
Truman <trumanngny@yahoo.com>
2021-11-28 16:37:23 -0500

    merge fixes

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -7,12 +7,14 @@ const postsController = require('./posts.js');
 const appConfigController = require('./appConfig.js');
 const userController = require('./user.js');
 const eventController = require('./event.js');
+const authController = require('./auth.js');
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
 router.use('/posts', postsController);
 router.use('/user', userController);
 router.use('/application-configuration', appConfigController);
 router.use('/event', eventController);
+router.use('/auth', authController);
 
 
 module.exports = router;
