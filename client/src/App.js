af1b143bb32f87ab832551b19190f0cf4d4e696c
sonya.wong <65000266+sonyaw612@users.noreply.github.com>
2021-11-30 22:29:37 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,11 @@ import PostsListPage from './pages/PostsListPage';
 import AboutUsPage from './pages/AboutUsPage';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
-
+import ProfilePage from './pages/ProfilePage';
+import Logo from './image/Logo.png';
+import { AuthProvider } from './context/AuthContext';
+import AuthButton from './components/AuthButton';
+import PrivateRoute from './components/PrivateRoute';
 import './App.css';
 import CreateEventPage from './pages/CreateEventPage';
 import EventPage from './pages/EventPage';
@@ -29,7 +33,7 @@ function Navigation(props) {
           </NavLink>
         </li>
         <li className="nav-item">
-          <NavLink className="nav-link" exact to="/posts/new">
+          <NavLink className="nav-link" exact to="/profile">
             Profile
           </NavLink>
         </li>
@@ -39,11 +43,12 @@ function Navigation(props) {
             About Us
           </NavLink>
         </li>
-        <li className="nav-item">
+        {/* <li className="nav-item">
           <NavLink className="nav-link" exact to="/sign-in">
             Sign in/Log Out
           </NavLink>
-        </li>
+        </li> */}
+        <AuthButton />
       </ul>
     </nav>
   );
@@ -54,7 +59,9 @@ class App extends React.Component {
   render() {
     return (
       <div class="container">
+        <img src={Logo}  width="1000" height="100"/>
         <div class="row">
+        <AuthProvider>
         <Router>
           <div class="col-sm-2 "> 
             <Navigation />
@@ -66,14 +73,18 @@ class App extends React.Component {
                 <Route path="/about-us" component={AboutUsPage} />
                 <Route path="/sign-in" component={SignInPage} />
                 <Route path="/sign-up" component={SignUpPage} />
+                <PrivateRoute path="/profile" component={ProfilePage} />
+                {/* <PrivateRoute path="/new-event" component={CreateEventPage} /> */}
                 <Route path="/new-event" component={CreateEventPage} />
                 <Route path="/event-page" component={EventPage}/>
+                {/* <Route path="/edit-event" component={EditEventPage}/> */}
                 <Route path="/" component={PostsListPage} />
               </Switch>
             
           
           </div>
         </Router>
+        </AuthProvider>
         </div>
         </div>
     );
