862bfb04789e7af829b0d1aa7f2921d309e44191
Truman <trumanngny@yahoo.com>
2021-12-01 00:01:03 -0500

    added attending model and controller

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -8,6 +8,7 @@ const appConfigController = require('./appConfig.js');
 const userController = require('./user.js');
 const eventController = require('./event.js');
 const authController = require('./auth.js');
+const attendingController = require('./attending.js');
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
 router.use('/posts', postsController);
@@ -15,6 +16,7 @@ router.use('/user', userController);
 router.use('/application-configuration', appConfigController);
 router.use('/event', eventController);
 router.use('/auth', authController);
+router.use('/attending', attendingController);
 
 
 module.exports = router;
---
+++ api/models/attending.js
@@ -0,0 +1,46 @@
+'use strict';
+const { Model } = require('sequelize');
+const { sequelize } = require('../../../learn-sequelize/models');
+
+module.exports = (sequelize, DataTypes) => {
+    class Attending extends Model {}
+
+    Event.init({
+        user_name: {
+            type: DataTypes.STRING,
+            // unique: true,
+            // // primaryKey: true,
+            // validate: {
+            //     len: [3, 250],
+            //     notEmpty: true,
+            // },
+        },
+        name: {
+            type: DataTypes.STRING,
+            // validate: {
+            //     len: [3, 250],
+            //     notEmpty: true,
+            // },
+        },
+        user_linkedln: {
+            type: DataTypes.STRING,
+           
+        },
+        user_image: {
+            type: DataTypes.STRING,
+           
+        },
+        event_name: {
+            type: DataTypes.STRING,
+            // validate: {
+            //     notEmpty: true,
+            // },
+        },
+       
+    }, { 
+        sequelize,
+        modelName: 'attending'
+     });
+
+    return Attending;
+};
