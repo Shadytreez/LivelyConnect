7715b5e585e088c628bcf3975bc8087b9b8d94e4
sonya.wong <65000266+sonyaw612@users.noreply.github.com>
2021-12-03 22:11:01 -0500

    Update EventPage.js

--- client/src/pages/EventPage.js
+++ client/src/pages/EventPage.js
@@ -47,32 +47,32 @@ class EventPage extends Component {
 
     // RSVP for event
     onClick = (event) => {
-
+        // add user to attendingTable
     }
 
     render() {
         return (
             <div style = {{ textAlign: 'center' }}>
                 <div style ={{ marginBottom: 50 }}>
-                    <img id = "banner" src = 'https://www.dia.org/sites/default/files/No_Img_Avail.jpg'
+                    <img id = "banner" src = {this.bannerImg}
                         style={{ height: 504.9, width: 897.6}}/>
                 </div>
                 <div className = "container" style = {{ marginBottom: 100 }}>
                     <div>
-                        <h2 id = 'eventName' size = '81'>This Is The Event Name</h2><br/><br/>
+                        <h2 id = 'eventName' size = '81'>This Is The Event Name {this.eventName} </h2><br/><br/>
                     </div>
                     <div className = "row row-cols-2">
                         <div className = "col"  style = {{ textAlign: 'left', marginLeft: 25 }}>
                             { /* Event name, description, activity type */}
-                            <p id = 'activityType' rows= '2' cols= '75'>This is the event's activity type.</p><br/>
-                            <p id = 'description' rows = '15' cols = '75'>This is the event description</p>
+                            <p id = 'activityType' rows= '2' cols= '75'>This is the event's activity type: {this.activityType}</p><br/>
+                            <p id = 'description' rows = '15' cols = '75'>This is the event description: {this.description}</p>
                         </div>
                         
                         <div className = "col">
                             { /* time of event, day, zoom link, rsvp button */}
                             <br/>
-                            <text id = 'dateTime' placeholder = '' type = 'date' >Date of event.</text><br/><br/>
-                            <a id = 'link' placeholder = 'Zoom Link' href = 'www.google.com'>Zoom link</a><br/><br/> {/* make zoom link taller */}
+                            <text id = 'dateTime' placeholder = '' type = 'date' >Date of event: {this.dateTime}</text><br/><br/>
+                            <a id = 'link' placeholder = 'Zoom Link' href = 'www.google.com'>Zoom link: {this.zoomLink}</a><br/><br/> {/* make zoom link taller */}
                             <button id = 'rsvp' onClick = {this.onClick} >RSVP Event</button>
                         </div>
                     </div>
