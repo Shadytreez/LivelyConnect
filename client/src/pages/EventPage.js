c5d1641fb29099b14cbac61f5b6782b1b4c5d909
sonya.wong <65000266+sonyaw612@users.noreply.github.com>
2021-12-05 23:44:21 -0500

    Merge branch 'main' into sonya

--- client/src/pages/EventPage.js
+++ client/src/pages/EventPage.js
@@ -1,20 +1,6 @@
 import React, { Component, useState } from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
-import { AuthContext } from '../context/AuthContext';
-import Popup from 'reactjs-popup';
-
-// const { Event } = require('../../../api/models/event');
-
-// POP-UP EXAMPLE:
-
-// <Popup trigger={<button> Trigger</button>} position="right center">
-//     <div>Popup content here !!</div>
-//   </Popup>
-
-=======
 import {Link} from 'react-router-dom';
->>>>>>> main
 class EventPage extends Component {
 
     state = {
@@ -28,12 +14,6 @@ class EventPage extends Component {
         hostUserName: '',
     }
 
-<<<<<<< HEAD
-    componentDidMount() {
-        fetch("/api/event/:id", {method: "GET"})
-        .then(res => res.json())
-        .then(data => {
-=======
 
 
     async componentDidMount() {
@@ -46,7 +26,6 @@ class EventPage extends Component {
         await fetch("/api/event/"+id)
         .then(response  => response.json())
         .then(responseJson  => {
->>>>>>> main
             this.setState({
                 // bannerImg, eventName, activityType, description, dateTime,
                 // zoomLink, isOpen, hostUserName
@@ -60,11 +39,7 @@ class EventPage extends Component {
                 isOpen: responseJson.isOpen, 
                 hostUserName: responseJson.hostUserName
             })
-<<<<<<< HEAD
-            console.log(data)
-=======
             console.log(responseJson.bannerImg)
->>>>>>> main
         })
     }
 
@@ -77,56 +52,26 @@ class EventPage extends Component {
         return (
             <div style = {{ textAlign: 'center' }}>
                 <div style ={{ marginBottom: 50 }}>
-<<<<<<< HEAD
-                    <img id = "banner" src = {this.bannerImg}
-=======
                     <img id = "banner" src = {this.state.bannerImg}
->>>>>>> main
                         style={{ height: 504.9, width: 897.6}}/>
                 </div>
                 <div className = "container" style = {{ marginBottom: 100 }}>
                     <div>
-<<<<<<< HEAD
-                        <h2 id = 'eventName' size = '81'>This Is The Event Name {this.eventName} </h2><br/><br/>
-=======
                         <h2 id = 'eventName' size = '81'>{this.state.eventName}</h2><br/><br/>
->>>>>>> main
                     </div>
                     <div className = "row row-cols-2">
                         <div className = "col"  style = {{ textAlign: 'left', marginLeft: 25 }}>
                             { /* Event name, description, activity type */}
-<<<<<<< HEAD
-                            <p id = 'activityType' rows= '2' cols= '75'>This is the event's activity type: {this.activityType}</p><br/>
-                            <p id = 'description' rows = '15' cols = '75'>This is the event description: {this.description}</p>
-=======
                             <p id = 'activityType' rows= '2' cols= '75'>{this.state.activityType}</p><br/>
                             <p id = 'description' rows = '15' cols = '75'>{this.state.description}</p>
->>>>>>> main
                         </div>
                         
                         <div className = "col">
                             { /* time of event, day, zoom link, rsvp button */}
                             <br/>
-<<<<<<< HEAD
-                            <text id = 'dateTime' placeholder = '' type = 'date' >Date of event: {this.dateTime}</text><br/><br/>
-                            <a id = 'link' placeholder = 'Zoom Link' href = 'www.google.com'>Zoom link: {this.zoomLink}</a><br/><br/> {/* make zoom link taller */}
-                            <Popup closeOnDocumentClick modal = {true} trigger={<button>Trigger</button>} position="top center"
-                                style={{
-                                    width: '100%',
-                                    textAlign: 'center',
-                                    padding: '5px',
-                                    display: 'block',
-                                    background: '#ffffff',
-                                    border
-                                }}>
-                                <div>List of Participants!</div>
-                            </Popup><br/>
-                            <button id = 'rsvp' onClick = {this.onClick} >RSVP Event</button>
-=======
                             <text id = 'dateTime' placeholder = '' type = 'date' >{this.state.dateTime}</text><br/><br/>
                             <a id = 'link' placeholder = 'Zoom Link' href = {this.state.zoomLink}>Zoom link</a><br/><br/> {/* make zoom link taller */}
                             <button id = 'rsvp'>RSVP Event</button>
->>>>>>> main
                         </div>
                     </div>
                 </div>
